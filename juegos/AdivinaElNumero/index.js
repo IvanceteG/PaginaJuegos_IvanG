602480feed40ea70411415064a5f693e440fcdc2
IvanceteG <159519799+IvanceteG@users.noreply.github.com>
2025-10-29 16:42:27 +0000

    Commit_PaginaPrincipal_Fondo

--- juegos/AdivinaElNumero/index.js
+++ juegos/AdivinaElNumero/index.js
@@ -1,13 +1,12 @@
 console.log("Hola Mundo!");
 
-let numeroAleatorio = Math.floor(Math.random() * 20 + 1);
+let numeroAleatorio = Math.floor(Math.random() * 100 + 1);
 const INTENTOS_MAX = 5;
 let btnComprobarNum = document.querySelector("#btnComprobarNum");
 let mensaje = document.querySelector("#mensajeComprobar");
 let inputNumero = document.querySelector("#inputNumero");
 let intentosRestantes = document.querySelector("#intentosRestantes");
 let intentos = INTENTOS_MAX;
-let btnReiniciarJuego = document.querySelector("#btnReiniciarJuego");
 
 
 
@@ -21,12 +20,14 @@ let numerosProbados = "";
 
 function comparar() {
     let numeroUsuario = parseInt(inputNumero.value);
-    if (numeroUsuario < 1 || numeroUsuario > 20) {
-        mensaje.innerHTML = "Por favor, introduce un número entre 1 y 20.";
+    if (numeroUsuario < 1 || numeroUsuario > 100) {
+        mensaje.innerHTML = "Por favor, introduce un número entre 1 y 100.";
     }else{
         if (numeroUsuario == numeroAleatorio){
-            mensaje.innerHTML = `¡Felicidades! Has adivinado el número. 
-            <button id="btnReiniciarJuego">Jugar de nuevo</button>`;
+            mensaje.innerHTML = `¡Felicidades! Has adivinado el número. <button id="btnReiniciarJuego">Jugar de nuevo</button>`;
+            let btnReiniciarJuego = document.querySelector("#btnReiniciarJuego");
+            btnReiniciarJuego.addEventListener("click", reiniciarJuego);
+            return;
         }
     } if (numeroUsuario < numeroAleatorio) {
         mensaje.innerHTML = "El número es mayor.";
@@ -41,19 +42,21 @@ function comparar() {
     }
     intentosRestantes.innerHTML = `Te quedan ${intentos} intentos.`;
     if (intentos <= 0) {
-        mensaje.innerHTML = `Lo siento. Has agotado todos tus intentos.`
+        mensaje.innerHTML = `Lo siento. Has agotado todos tus intentos. El número era el ${numeroAleatorio}.`;
+        mensaje.innerHTML += ` <button id="btnReiniciarJuego">Jugar de nuevo</button>`;
+        let btnReiniciarJuego = document.querySelector("#btnReiniciarJuego");
+        btnReiniciarJuego.addEventListener("click", reiniciarJuego);
     }
-    
-    btnReiniciarJuego.addEventListener("click", reiniciarJuego);
+}
 
-    function reiniciarJuego() {
-    numeroAleatorio = Math.floor(Math.random() * 20 + 1);
+function reiniciarJuego() {
+    numeroAleatorio = Math.floor(Math.random() * 100 + 1);
+    inputNumero.value = "";
     intentos = INTENTOS_MAX;
     numerosProbados = "";
     mensaje.innerHTML = `Vamos a jugar!!! Haber si puedes adivinarlo en 5 intentos.`;
     intentosRestantes.innerHTML = `Te quedan ${intentos} intentos.`;
     document.querySelector("#numerosProbados").innerHTML = `Numeros probados: ${numerosProbados}`;
-}
 }
 
 
